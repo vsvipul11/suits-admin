17aa4c53070bd3353a7b48cf51f4ee55568b9d14
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2023-11-10 17:14:28 +0530

    Add files via upload

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import OTPVerification from './OtpVerification';
 import './Login.scss'; // Import the CSS file
-import login from '../src/images/login.avif';
 
 const Login = () => {
   const [userId, setUserId] = useState('');
   const [showOTPField, setShowOTPField] = useState(false);
   const [otp, setOTP] = useState('');
+  const login = 'https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=';
 
   const handleGenerateOTP = () => {
     axios
